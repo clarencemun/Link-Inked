ea6f938a93cb1ba3645eeaa4dc159d2a5066edb9
clarencemun <clarence.mun@gmail.com>
2024-11-14 23:45:14 +0800

    Update Link-Inked.py

--- Link-Inked.py
+++ Link-Inked.py
@@ -8,10 +8,8 @@ import uuid
 import streamlit.components.v1 as components
 import requests
 from bs4 import BeautifulSoup
-import openai
+from openai import AzureOpenAI
 import re
-
-client = OpenAI()
 
 # Set base directory
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -42,8 +40,12 @@ with st.sidebar:
 
 # Azure OpenAI setup (only if Cloud is selected)
 if model_type == 'Cloud':
-    os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-    client = OpenAI()
+    os.environ["AZURE_OPENAI_API_KEY"] = st.secrets["AZURE_OPENAI_API_KEY"]
+    client = AzureOpenAI(
+        azure_endpoint=st.secrets["AZURE_ENDPOINT"],
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        api_version=st.secrets["AZURE_API_VERSION"]
+    )
 
 costar_prompt = """
 # CONTEXT #
@@ -109,7 +111,7 @@ def generate_azure_comment(article_content):
     prompt = f"{costar_prompt}\n[ARTICLE]\n{article_content}\n"
     try:
         response = client.chat.completions.create(
-            model="gpt-4o",
+            model="gpt-4-0125-preview",
             messages=[{'role': 'user', 'content': prompt}],
             temperature=0.7  # Increase temperature for more creative and varied responses
         )
@@ -299,7 +301,7 @@ Print only the improved LinkedIn comment and nothing but the improved LinkedIn c
                 improved_comment = improved_comment.strip()
             else:
                 response = client.chat.completions.create(
-                    model="gpt-4o",
+                    model="gpt-4-0125-preview",
                     messages=[{'role': 'user', 'content': improve_prompt}],
                     temperature=0.7
                 )
