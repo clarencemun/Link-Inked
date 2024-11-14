b1f9733249d335aadc643a9b9145d89b038ebf74
clarencemun <clarence.mun@gmail.com>
2024-11-14 23:47:34 +0800

    Update Link-Inked.py

--- Link-Inked.py
+++ Link-Inked.py
@@ -8,8 +8,12 @@ import uuid
 import streamlit.components.v1 as components
 import requests
 from bs4 import BeautifulSoup
-from openai import AzureOpenAI
+import openai
 import re
+
+# Initialize the OpenAI client
+client = openai.OpenAI()
+os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
 
 # Set base directory
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -40,12 +44,8 @@ with st.sidebar:
 
 # Azure OpenAI setup (only if Cloud is selected)
 if model_type == 'Cloud':
-    os.environ["AZURE_OPENAI_API_KEY"] = st.secrets["AZURE_OPENAI_API_KEY"]
-    client = AzureOpenAI(
-        azure_endpoint=st.secrets["AZURE_ENDPOINT"],
-        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-        api_version=st.secrets["AZURE_API_VERSION"]
-    )
+    os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
+    client = openai.OpenAI()
 
 costar_prompt = """
 # CONTEXT #
@@ -110,8 +110,8 @@ def generate_comment(article_content):
 def generate_azure_comment(article_content):
     prompt = f"{costar_prompt}\n[ARTICLE]\n{article_content}\n"
     try:
-        response = client.chat.completions.create(
-            model="gpt-4-0125-preview",
+        response = client.chat_completions.create(
+            model="gpt-4o",
             messages=[{'role': 'user', 'content': prompt}],
             temperature=0.7  # Increase temperature for more creative and varied responses
         )
@@ -300,8 +300,8 @@ Print only the improved LinkedIn comment and nothing but the improved LinkedIn c
                         improved_comment += chunk['message']['content']
                 improved_comment = improved_comment.strip()
             else:
-                response = client.chat.completions.create(
-                    model="gpt-4-0125-preview",
+                response = client.chat_completions.create(
+                    model="gpt-4o",
                     messages=[{'role': 'user', 'content': improve_prompt}],
                     temperature=0.7
                 )
